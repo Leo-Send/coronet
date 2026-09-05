27ad485c4d351e2fc84f2716527afb599efbec75
Christian Hechtl <hechtl@cs.uni-saarland.de>
2026-09-05 15:13:44 +0200

    Remove test filter for final test
    Signed-off-by: Christian Hechtl <hechtl@cs.uni-saarland.de>

--- tests.R
+++ tests.R
@@ -50,7 +50,7 @@ requireNamespace("patrick")
 
 ## starting tests
 do.tests = function(dir) {
-    res = testthat::test_dir(dir, reporter = "check", filter = "core")
+    res = testthat::test_dir(dir, reporter = "check")
     if (length(res[["failures"]]) > 0) {
         cat(str_c("Some R tests failed for directory '", dir, "'"))
 
